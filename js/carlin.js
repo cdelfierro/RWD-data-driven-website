82a901f700f85e50bd8f29b192fa2810836531a6
Carlos Gómez del Fierro <cdelfierro@gmail.com>
2016-04-01 21:58:06 -0300

    Agrega primer template

--- js/carlin.js
+++ js/carlin.js
@@ -1,1 +1,24 @@
-// put your javascript code here
+// javascript code
+
+var categories_template;
+
+function show_template(template, data) {
+    var html = template(data);
+    $("#content").html(html);
+}
+
+$(document).ready(function() {
+
+    var source = $("#categories-template").html();
+    categories_template = Handlebars.compile(source);
+
+    $("#categories-tab").click(function() {
+
+        show_template(categories_template, animals_data);
+        $(".nav-tabs .active").removeClass("active");
+        $("#categories-tab").addClass("active");
+    });
+
+    $("#categories-tab").click();
+
+});
