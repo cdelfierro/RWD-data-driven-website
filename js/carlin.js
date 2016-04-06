e3bca948dcc825093dc1b8161a80cb02982713ff
Carlos Gómez del Fierro <cdelfierro@gmail.com>
2016-04-05 21:39:27 -0300

    Agrega la funcionalidad a la pestaña 'Description'

--- js/carlin.js
+++ js/carlin.js
@@ -1,7 +1,7 @@
 // javascript code
 
 // variables for all templates
-var categories_template, animals_template, photos_template;
+var categories_template, gallery_template, photos_template;
 
 // current variables to sabe between the clics of differents sections
 var current_category = null;
@@ -18,10 +18,10 @@ $(document).ready(function() {
     var source = $("#categories-template").html();
     categories_template = Handlebars.compile(source);
 
-    source = $("#animals-template").html();
-    animals_template = Handlebars.compile(source);
+    source = $("#gallery-template").html();
+    gallery_template = Handlebars.compile(source);
 
-    source = $("#photos-template").html();
+    source = $("#photo-template").html();
     photos_template = Handlebars.compile(source);
 
     // Clicking in the categories tab shows all the categories.
@@ -40,27 +40,52 @@ $(document).ready(function() {
 
     // Clicking in the gallery tab shows the gallery of the current category.
     $("#gallery-tab").click(function() {
-        console.log(current_category);
         if (current_category) {
-            show_template(animals_template, current_category);
-        } else {
-            $("#categories-tab").click();
-        }
-        $(".nav-tabs .active").removeClass("active");
-        $("#gallery-tab").addClass("active");
+            show_template(gallery_template, current_category);
+            $(".nav-tabs .active").removeClass("active");
+            $("#gallery-tab").addClass("active");
 
-        // Clicking the animal thumbnail carry us to the image and description.
-        $(".animal-thumb").click(function() {
-            console.log(current_animal);
-            var index = $(this).data("id");
-            current_animal = current_category.animals[index];
+            // Clicking the animal thumbnail carry us to the image and description.
+            $(".animal-thumb").click(function() {
+                var index = $(this).data("id");
+                current_animal = current_category.animals[index];
+                $("#photo-tab").click();
+            });
+        } else {
+            show_template(categories_template, animals_data);
+            $(".nav-tabs .active").removeClass("active");
+            $("#categories-tab").addClass("active");
+            $("#category-list").append("<div class='alert alert-warning' role='alert'>Please, select a category before</div>");
+            $(".list-group-item").click();
+
+            // Clicking in the categories listed shows the animnals in the category
+            $(".list-group-item").click(function() {
+                var index = $(this).data("id");
+                current_category = animals_data.category[index];
+                $("#gallery-tab").click();
+            });
+        }
+    });
+
+    // Clicking in the photo tab shows te photo of the current animal
+    $("#photo-tab").click(function() {
+        if (current_animal) {
             show_template(photos_template, current_animal);
+            $(".nav-tabs .active").removeClass("active");
+            $("#photo-tab").addClass("active");
 
             // Clicking the photo carry us back to the gallery.
             $(".animal-photo").click(function() {
                 $("#gallery-tab").click();
             });
-        });
+        } else if (current_category) {
+            show_template(gallery_template, current_category);
+            $(".nav-tabs .active").removeClass("active");
+            $("#gallery-tab").addClass("active");
+            $("#gallery").append("<div class='col-xs-12'><div class='alert alert-warning' role='alert'>Please, select an animal to view its details.</div></div>");
+        } else {
+            $("#gallery-tab").click();
+        }
     });
 
     $("#categories-tab").click();
